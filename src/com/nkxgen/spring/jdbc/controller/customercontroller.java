934a30a45cc9ea31a1009f6b0304a7d58899e604
NeerajPameti <135199071+NeerajPameti@users.noreply.github.com>
2023-06-16 07:51:31 +0530

    Add files via upload

---
+++ src/com/nkxgen/spring/jdbc/controller/customercontroller.java
@@ -0,0 +1,67 @@
+package com.nkxgen.spring.jdbc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.nkxgen.spring.jdbc.model.Customer;
+import com.nkxgen.spring.jdbc.model.CustomerDao;
+import com.nkxgen.spring.jdbc.model.Customertrail;
+import com.nkxgen.spring.jdbc.model.service;
+
+@Controller
+public class customercontroller {
+	@Autowired
+	private CustomerDao customer1;
+
+	@RequestMapping(value = "/customer_data_trail_save", method = RequestMethod.POST)
+	public String customer_data_trail_save(@Validated Customertrail customer, Model model) {
+		customer1.save_trail(customer);
+		return "Account_new_application_form";
+	}
+
+	// =================================================
+	// real saving
+	@RequestMapping(value = "/save_to_customer_database", method = RequestMethod.POST)
+	public String save_to_customer_database(@RequestParam("CustomerId") Long customerId) {
+		long customerid = customerId;
+		System.out.println(customerid);
+		Customertrail cus = customer1.getCustomerById(customerid);
+		Customer customer = service.dotheservice(cus);
+		customer1.deleteCusById(cus);
+		customer1.save(customer);
+		// change the return
+		return "Account_new_application_form";
+	}
+
+	// =========================================================
+	@RequestMapping(value = "/customers", method = RequestMethod.POST)
+	public String customers(Model model) {
+
+		List<Customer> customerList = customer1.getAllCustomers();
+		model.addAttribute("customerList", customerList);
+		return "customer_edit_details_form";
+
+	}
+
+	// ===============================================================
+	// editing the existing data
+	// =========================================================
+	@RequestMapping(value = "/customer_data_updation", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<String> customerDataUpdation(Customer updatedCustomer) {
+		// Assuming customer1 is an instance of your CustomerRepository or service
+		System.out.println(updatedCustomer);
+		customer1.updateCustomerDataById(updatedCustomer);
+		return ResponseEntity.ok("Customer data updated successfully");
+	}
+
+}
